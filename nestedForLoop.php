00655f66e140175729b5c3b640ea438cb310f4ed
SadiasHossain <158283811+SadiasHossain@users.noreply.github.com>
2024-10-02 10:42:00 +0100

    step 3

--- nestedForLoop.php
+++ nestedForLoop.php
@@ -1,12 +1,12 @@
 <?php
-
-for ($i=1;$i<=20;$i++) {
-    echo"$i, ";
-    if (($i % 5)==0) {
-        echo "<BR>";
+echo "<table border = '1 '>";
+for ($i=1;$i<=15;$i++) {
+    echo "<TR>";
+    for ($j = 1; $j <= 20; $j++) {
+        echo "<td>Row $i, column $j</TD>";
     }
 
-    
+    echo "</TR>";
 }
-
+echo "</TABLE>";
 ?>
